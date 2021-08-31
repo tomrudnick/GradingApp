48fd56e494369ed366287c1bc17d7cf0d69bf499
Matthias Rudnick <matthias@rudnick.ch>
2021-08-31 12:22:47 +0200

    Added import/export functionallity.

--- GradingApp/ViewModels/MoreActionsViewModel.swift
+++ GradingApp/ViewModels/MoreActionsViewModel.swift
@@ -50,19 +50,72 @@ class MoreActionsViewModel: ObservableObject {
     }
     
     
-    func getDocuments(viewContext: NSManagedObjectContext) -> [CSVFile] {
+    func getDocumentsSingleFiles(viewContext: NSManagedObjectContext) -> [CSVFile] {
         let fetchedCourses = PersistenceController.fetchData(context: viewContext, fetchRequest: Course.fetchAll())
         var files: [CSVFile] = []
+        let date = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd_MM_YYYY"
+        let fileDate = dateFormatter.string(from: date)
         for course in fetchedCourses {
             let oralRequest  = Grade.fetch(NSPredicate(format: "type = %d AND student.course = %@", 0, course))
             let oralGrades = PersistenceController.fetchData(context: viewContext, fetchRequest: oralRequest)
-            files.append(CSVFile.generateCSVFileOFCourse(course: course, grades: oralGrades, fileName: "\(course.title)_muendlich"))
+            files.append(CSVFile.generateCSVFileOFCourse(course: course, grades: oralGrades, fileName: "\(course.title)_muendlich_"+fileDate))
             let writtenRequest  = Grade.fetch(NSPredicate(format: "type = %d AND student.course = %@", 1, course))
             let writtenGrades = PersistenceController.fetchData(context: viewContext, fetchRequest: writtenRequest)
-            files.append(CSVFile.generateCSVFileOFCourse(course: course, grades: writtenGrades, fileName: "\(course.title)_schriftlich"))
+            files.append(CSVFile.generateCSVFileOFCourse(course: course, grades: writtenGrades, fileName: "\(course.title)_schriftlich_"+fileDate))
         }
         return files
+        
     }
+    
+    func backup(url: URL){
+        let container = NSPersistentContainer(name: "GradingAppData")
+        try! container.copyPersistentStores(to: url)
+    }
+    
+    func restore(url: URL){
+        let container = NSPersistentContainer(name: "GradingAppData")
+        container.loadPersistentStores { description, error in
+                    if let error = error {
+                        fatalError("Unable to load persistent stores: \(error)")
+                    }
+                }
+        try! container.restorePersistentStore(from: url)
+        
+    }
+   
+    
+    /*func backup(url: URL) {
+            let backUpFolderUrl = url
+            let backupUrl = backUpFolderUrl.appendingPathComponent("GradingApp_Backup" + ".txt")
+            let container = NSPersistentContainer(name: "GradingAppData")
+            container.loadPersistentStores(completionHandler: { (storeDescription, error) in })
+            let store:NSPersistentStore
+            store = container.persistentStoreCoordinator.persistentStores.last!
+            do {
+                try container.persistentStoreCoordinator.migratePersistentStore(store,to: backupUrl,options: nil,withType: NSSQLiteStoreType)
+            } catch {
+                print("Failed to migrate")
+            }
+        }
+    
+    func restore (url: URL, viewContext: NSManagedObjectContext){
+            let storeFolderUrl = FileManager.default.urls(for: .applicationSupportDirectory, in:.userDomainMask).first!
+            let storeUrl = storeFolderUrl.appendingPathComponent("GradingAppData.sqlite")
+            let backupUrl = url
+            let container = NSPersistentContainer(name: "GradingAppData")
+            container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+                do{
+                    try container.persistentStoreCoordinator.replacePersistentStore(at: storeUrl,destinationOptions: nil,withPersistentStoreFrom: backupUrl,sourceOptions: nil,ofType: NSSQLiteStoreType)
+                } catch {
+                    print("Failed to restore")
+                }
+
+            })
+        }*/
+        
+    
     
     func done() {
         let df = DateFormatter()
@@ -81,6 +134,15 @@ class MoreActionsViewModel: ObservableObject {
         } else {
             return false
         }
+    }
+    
+    func deleteAllCourses(viewContext: NSManagedObjectContext) {
+        let fetchedCourses = PersistenceController.fetchData(context: viewContext, fetchRequest: Course.fetchAll())
+        for course in fetchedCourses {
+            viewContext.delete(course)
+            viewContext.saveCustom()
+        }
+        
     }
     
     
