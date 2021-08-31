d8b5e483f784f449f5d9d3c79a422fedcf92c4a6
Tom Rudnick <>
2021-08-31 12:25:55 +0200

    TranscriptGradeprotocol

--- GradingApp/Models/CoreData/Grade/TranscriptGrade+helper.swift
+++ GradingApp/Models/CoreData/Grade/TranscriptGrade+helper.swift
@@ -14,3 +14,21 @@ extension HalfYearTranscriptGrade {
         set { half_ = newValue.rawValue }
     }
 }
+
+
+protocol TranscriptGradeProtocol {
+    func getTranscriptValueString() -> String
+    func getTranscriptSetKeyPath() -> KeyPath<TranscriptGrade, Int>
+}
+
+extension HalfYearTranscriptGrade : TranscriptGradeProtocol {
+    func getTranscriptValueString() -> String {
+        return "\(half == .firstHalf ? "1. " : "2. ") Halbjahr Note: \(value)"
+    }
+    
+    func getTranscriptSetKeyPath() -> KeyPath<TranscriptGrade, Int> {
+        <#code#>
+    }
+    
+    
+}
