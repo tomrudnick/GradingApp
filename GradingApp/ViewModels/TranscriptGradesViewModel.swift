21c18dfed33153dd2e31eda45e7e24392fc92cb9
Tom Rudnick <>
2021-09-01 00:37:47 +0200

    Added TransriptGradesViewModel

---
+++ GradingApp/ViewModels/TranscriptGradesViewModel.swift
@@ -0,0 +1,43 @@
+//
+//  TranscriptGradesViewModel.swift
+//  GradingApp
+//
+//  Created by Tom Rudnick on 01.09.21.
+//
+
+import Foundation
+import CoreData
+
+class TranscriptGradesViewModel : ObservableObject {
+    @Published var studentGrades: [GradeStudent<TranscriptGrade>]
+    private(set) var course: Course?
+    
+    init() {
+        self.course = nil
+        self.studentGrades = []
+    }
+    
+    func fetchData(course: Course) {
+        self.course = course
+        self.studentGrades = course.studentsArr
+            .filter({$0.transcriptGrade != nil})
+            .map({ student in
+                GradeStudent(student: student, grade: student.transcriptGrade)
+            }).sorted(by: {$0.student.firstName < $1.student.lastName})
+              .sorted(by: {$0.student.lastName < $1.student.lastName})
+    }
+    
+    func setGrade(for student: Student, value: Int) {
+        GradeStudent<TranscriptGrade>.setGrade(studentGrades: &studentGrades, for: student, value: value)
+    }
+    
+    func save(viewContext: NSManagedObjectContext) {
+        for studentGrade in studentGrades {
+            //since we filtered the grades for non nil objects this should allways succeed
+            if let grade = studentGrade.grade {
+                grade.value = Int32(studentGrade.value)
+            }
+            viewContext.saveCustom()
+        }
+    }
+}
