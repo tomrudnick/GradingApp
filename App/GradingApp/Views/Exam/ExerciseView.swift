c2b4c25b0e17a19b745621e8f99051a11edba90a
Tom Rudnick <tom@rudnick.ch>
2022-11-03 15:30:22 +0100

    Textfield for ExamViews part 2

--- App/GradingApp/Views/Exam/ExerciseView.swift
+++ App/GradingApp/Views/Exam/ExerciseView.swift
@@ -8,13 +8,25 @@
 import SwiftUI
 
 struct ExerciseView: View {
+    
+    @ObservedObject var examVM: ExamViewModel
+    @ObservedObject var exercise: ExamViewModel.ExerciseVM
+    
+    @State var textField: String = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack {
+                Text("Exercise Name: ")
+                TextField("Aufgabe...", text: $exercise.title)
+                    .onChange(of: exercise.title) { _ in
+                        examVM.objectWillChange.send()
+                    }
+                    
+            }
+            TextField("..", text: $textField)
+            Spacer()
+        }
     }
 }
 
-struct ExerciseView_Previews: PreviewProvider {
-    static var previews: some View {
-        ExerciseView()
-    }
-}
